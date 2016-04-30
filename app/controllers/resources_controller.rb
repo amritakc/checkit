a298afc7a2bbcd0c7cf48d4b2360fa7689bb006a
amritakc <amritakc@gmail.com>
2016-04-30 15:44:12 -0700

    added mental health controller and page

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -33,6 +33,65 @@ class ResourcesController < ApplicationController
 	end
 
 
+	def show_hospital
+
+			#creating the params for the geocoder
+		params = {
+           # category_filter: 'hospital',
+           term: 'hospital',
+           limit: 5,
+           # offset: 5,
+           radius_filter: 6000
+        }
+
+
+		locale = { cc: "US", lang: 'en' }
+
+		# @center = Geocoder::Calculations.geographic_center(session[:addresses])
+		coordinates = {latitude:37.784558, longitude: -122.398564}
+		@data = Yelp.client.search_by_coordinates(coordinates,params, locale).businesses
+		# render :text => @data
+
+
+			@hash = Gmaps4rails.build_markers(@data) do |place, marker|
+				@placeinfo = "<p>" + place.name + "</p><p>" + place.display_phone.gsub("+1-","") + "</p>"
+ 				marker.lat place.location.coordinate.latitude
+ 	 			marker.lng place.location.coordinate.longitude
+ 	 			marker.infowindow @placeinfo 
+			end
+
+	end
+
+	def show_mentalhealth
+
+			#creating the params for the geocoder
+		params = {
+           category_filter: 'c_and_mh',
+           
+           limit: 10,
+           # offset: 5,
+           radius_filter: 6000
+        }
+
+
+		locale = { cc: "US", lang: 'en' }
+
+		# @center = Geocoder::Calculations.geographic_center(session[:addresses])
+		coordinates = {latitude:37.784558, longitude: -122.398564}
+		@data = Yelp.client.search_by_coordinates(coordinates,params, locale).businesses
+		# render :text => @data
+
+
+			@hash = Gmaps4rails.build_markers(@data) do |place, marker|
+				@placeinfo = "<p>" + place.name + "</p><p>" + place.display_phone.gsub("+1-","") + "</p>"
+ 				marker.lat place.location.coordinate.latitude
+ 	 			marker.lng place.location.coordinate.longitude
+ 	 			marker.infowindow @placeinfo 
+			end
+
+	end
+
+
 	
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   post '/sessions' => 'users#create'
   get '/homepage/:id' => 'resources#index'
   get '/food' => 'resources#show_food'
-
+  get '/hospital' => 'resources#show_hospital'
+  get '/mental_health' => 'resources#show_mentalhealth'
 end
