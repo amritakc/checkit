dc2982f8a1c9d853b62694602353a136fb676976
Heather Walker <cookiesncream@ladle.net>
2016-04-30 18:21:43 -0700

    food/hospital link go to new tab

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,8 @@ class UsersController < ApplicationController
 
 	  		if @user && @user.authenticate(params[:password])
 	      		session[:user_id] = @user.id
-	  			redirect_to '/homepage/%d' % @user.id
+	      		redirect_to '/homepage'
+	  			# redirect_to '/homepage/%d' % @user.id
 	  		else
 	  			flash[:loginmistakes] = ["Invalid Login"]
 	  			redirect_to "/" 
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   
   root 'users#index'
   post '/sessions' => 'users#create'
-  get '/homepage/:id' => 'resources#index'
+  get '/homepage' => 'resources#index'
   get '/food' => 'resources#show_food'
   get '/hospital' => 'resources#show_hospital'
   get '/mental_health' => 'resources#show_mentalhealth'
