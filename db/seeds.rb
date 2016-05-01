697468966c9d9ea94708e5d2b90421abf3aaea89
Heather Walker <cookiesncream@ladle.net>
2016-05-01 12:52:41 -0700

    added data in seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Youtshelter.create(name:"Lark Inn", street:"869 Ellis St", city:"San Francisco", state:"CA", latitude:"37.783680", longitude:"-122.420228", phone:"415-749-2968") 
+Youtshelter.create(name:"Huckleberry House", street:"1292 Page St", city:"San Francisco", state: "CA", latitude:"37.771795", longitude:"-122.442074", phone:"415-621-2929")
+Youtshelter.create(name:"Diamond Youth Shelter", street:"536 Central", city:"San Francisco", state: "CA", latitude:"37.774728", longitude:"-122.444283", phone:"415-567-1020")
+
+Womenshelter.create(name:"Women's Emergency Shelter", street:"916 Laguna St, Suite 225", city:"San Francisco",state: "CA", latitude:"37.779931", longitude:"-122.426663", phone:"415-684-0546")
+Womenshelter.create(name:"Rosalie House", street:"1175 Howard Street, Suite 225",city: "San Francisco",state: "CA", latitude:"37.776611", longitude:"-122.410705",phone:"415-255-0165")
+Womenshelter.create(name:"La Casa de las Madres", street:"1663 Mission, Suite 225", city:"San Francisco",state: "CA", latitude:"37.771231", longitude:"-122.419317",phone:"415-503-0500")
+Womenshelter.create(name:"Asia Women's Shelter", street:"3543 18th St #19",city: "San Francisco", state:"CA", latitude:"37.761440", longitude:"-122.422656",phone:"415-751-0880")
+
+User.create(email:"ali@gmail.com", password:"password")
