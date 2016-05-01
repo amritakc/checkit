ff9aff01b2adb78eff7160db89312a14dbc6cf06
Sonya <sonyapanich@gmail.com>
2016-05-01 11:56:48 -0700

    Adding html

--- app/controllers/forms_controller.rb
+++ app/controllers/forms_controller.rb
@@ -3,9 +3,4 @@ class FormsController < ApplicationController
 	def index
 	end
 
-	def create
-		Form.create(gender:params[:gender], age:params[:age], contact_point:params[:contact_point], previous_meeting:params[:previous_meeting], responder_name:params[:responder_name])
-		redirect_to '/homepage'
-	end
-
 end
