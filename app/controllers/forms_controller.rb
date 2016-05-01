eee92c700b6155f2f2057bc2e4fadfd6470945aa
amritakc <amritakc@gmail.com>
2016-05-01 10:59:19 -0700

    created create method in forms controller

--- app/controllers/forms_controller.rb
+++ app/controllers/forms_controller.rb
@@ -4,7 +4,8 @@ class FormsController < ApplicationController
 	end
 
 	def create
-		Form.create()
+		Form.create(gender:params[:gender], age:params[:age], contact_point:params[:contact_point], previous_meeting:params[:previous_meeting], responder_name:params[:responder_name])
+		redirect_to '/homepage'
 	end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
 
 		def create
 	  		@user = User.find_by(email: params[:email])
+	  		session[:user_id]=@user.id
 
 	  		if @user && @user.authenticate(params[:password])
 	      		session[:user_id] = @user.id
